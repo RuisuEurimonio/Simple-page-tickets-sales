6e275c3a930d7473899d28107f701668e059763e
Ruisu Eurimonio <pipelinares2912@gmail.com>
2022-02-19 16:49:41 -0500

    scripts ready, with the validation, event and alerts

--- Scripts/buttonListener.js
+++ Scripts/buttonListener.js
@@ -1,22 +1,48 @@
 var button = document.getElementById("sendInfo");
 button.addEventListener("click", getData);
 
-function getData(){
-    document.preventDefault();
-    let cityOrigin = document.getElementById("cityOrigin");
-    let cityDestiny = document.getElementById("cityDestiny");
-    let date = document.getElementById("date");
-    let hour = document.getElementById("hour");
-    let placeNumber = document.getElementById("placeNumber");
-    let identification = document.getElementById("identification");
-    let name = document.getElementById("name");
+function getData(event){
+    event.preventDefault();
+    let cityOrigin = document.getElementById("cityOrigin").value;
+    let cityDestiny = document.getElementById("cityDestiny").value;
+    let date = document.getElementById("date").value;
+    let hour = document.getElementById("hour").value;
+    let placeNumber = document.getElementById("placeNumber").value;
+    let identification = document.getElementById("identification").value;
+    let name = document.getElementById("name").value;
 
-    let strings = validationStr(cityOrigin, cityDestiny, name);
-    let numbers = validationInt(placeNumber, identification);
+    let length = dataLength(cityOrigin, cityDestiny,date,hour,placeNumber, identification, name);
 
-    if(strings && numbers && (date.length() > 0) && (hour.length() > 0)){
-        console.log("sisas");
+    if(length){
+        let strings = validationStr(cityOrigin, cityDestiny, name);
+        let numbers = validationInt(placeNumber, identification);
+        if(strings && numbers){
+            cleanInputs();
+            alertSuccessfull();
+        } else {
+            alert("Hay datos que no cumplen con la forma solicitada.")
+        }
     } else {
-        console.log("nokas");
+        alert("Hay datos que faltan o que no cumplen con el tamaño ideal.");
     }
+}
+
+function cleanInputs(){
+    document.getElementById("cityOrigin").value = "";
+    document.getElementById("cityDestiny").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("hour").value = "";
+    document.getElementById("placeNumber").value = "";
+    document.getElementById("identification").value = "";
+    document.getElementById("name").value = "";
+}
+
+function alertSuccessfull(){
+    let alert = document.getElementById("alertForm");
+    alert.classList.toggle("invisible");
+    alert.classList.toggle("visible");
+    setTimeout(()=>{
+        alert.classList.toggle("invisible");
+        alert.classList.toggle("visible");
+    }, 6000);
 }
--- Scripts/validations.js
+++ Scripts/validations.js
@@ -1,8 +1,30 @@
-function validationStr(cityOrigin, cityDestiny, name){
-    let cities = new RegExp('[a-zA-Z ]{4,30}', 'g');
-    let names = new RegExp('[a-z ]{10,50}', 'gi');
+function dataLength(cityO, cityD, date, hour, placeN, id, name){
+    let cityOL = cityO.length;
+    let cityDL = cityD.length;
+    let dateL = date.length;
+    let hourL = hour.length;
+    let idL = id.length;
+    let nameL = name.length;
 
-    if(cities.test(cityOrigin) && cities.test(cityDestiny) && names.test(name)){
+    if(cityOL > 0 && cityOL < 30 && cityDL > 0 && cityDL < 30 
+        && dateL > 0 && hourL > 0 && placeN > 0 && placeN < 80 
+        && idL > 5 && idL < 13 && nameL > 10 && nameL < 51){
+            return true;
+        } else {
+            return false;
+        }
+}
+
+function validationStr(cityOrigin, cityDestiny, name){
+    let origins = new RegExp('[a-zA-Z ]{4,30}', 'g');
+    let destinies = new RegExp('[a-zA-Z ]{4,30}', 'g');
+    let names = new RegExp('[a-zA-Z ]{10,50}', 'g');
+
+    let res1 = origins.test(cityOrigin);
+    let res2 = destinies.test(cityDestiny);
+    let res3 = names.test(name);
+
+    if(res1 && res2 && res3){
         return true;
     } else {
         return false;
@@ -13,7 +35,10 @@ function validationInt(placeNumber, identificacion){
     let numbers = new RegExp('[0-9]{1,80}','g');
     let idRegExp = new RegExp('[0-9]{5,12}','g');
 
-    if(numbers.test(placeNumber) && idRegExp.test(identificacion)){
+    let res1 = numbers.test(placeNumber);
+    let res2 = idRegExp.test(identificacion);
+
+    if(res1 && res2){
         return true;
     } else {
         return false;
